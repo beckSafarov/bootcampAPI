ddbff64c6c4266ad22167657162a681d56156d04
Begzod Safarov <begzod.safarov07@gmail.com>
2020-09-07 10:08:16 +0800

    Added routing functions

--- server.js
+++ server.js
@@ -6,6 +6,29 @@ const express = require('express'),
 //load env vars
 dotenv.config({ path: './config/config.env' });
 
+app.get('/api/v1/bootcamps', (request, response) => {
+  response.json({ success: true, msg: 'Show all bootcamps' });
+});
+
+app.get('/api/v1/bootcamps/:id', (request, response) => {
+  response.json({ success: true, msg: 'Show bootcamp ' + request.params.id });
+});
+
+app.post('/api/v1/bootcamps', (request, response) => {
+  response.json({ success: true, msg: 'Create a new bootcamp' });
+});
+
+app.put('/api/v1/bootcamps/:id', (request, response) => {
+  response.json({
+    success: true,
+    msg: `Update bootcamp ${request.params.id}`,
+  });
+});
+
+app.delete('/api/v1/bootcamps/:id', (request, response) => {
+  response.json({ success: true, msg: `Delete bootcamp ${request.params.id}` });
+});
+
 app.listen(
   PORT,
   console.log(
